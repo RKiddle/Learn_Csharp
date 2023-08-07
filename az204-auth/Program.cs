b2bbaae59757f2dde08a2e9481c310a1cff0f4f1
RKIddle <asiatutors@gmail.com>
2023-08-07 10:57:07 +0700

    Implement interactive authentication by using MSAL.NET

---
+++ az204-auth/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Identity.Client;
+
+namespace az204_auth
+{
+    class Program
+    {
+        private const string _clientId = "b98620fc-3e7c-418b-a4f0-6457b5548aa4";
+        private const string _tenantId = "8e8e0923-9fd1-48fc-b274-b5373cb4026f";
+
+        public static async Task Main(string[] args)
+        {
+            var app = PublicClientApplicationBuilder
+                .Create(_clientId)
+                .WithAuthority(AzureCloudInstance.AzurePublic, _tenantId)
+                .WithRedirectUri("http://localhost")
+                .Build(); 
+            string[] scopes = { "user.read" };
+            AuthenticationResult result = await app.AcquireTokenInteractive(scopes).ExecuteAsync();
+
+            Console.WriteLine($"Token:\t{result.AccessToken}");
+        }
+    }
+}
