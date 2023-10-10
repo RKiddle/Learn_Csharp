882da1d2e516644cbe78cb5309a82ecc3049a251
RKIddle <asiatutors@gmail.com>
2023-10-10 09:17:19 +0700

    cin toss program

---
+++ Logic/coin-toss/Program.cs
@@ -0,0 +1,12 @@
+﻿// Coin toss program
+// Author: Richard Kiddle   
+// Date: 10/10/23
+Random rand = new();
+int coin_probabilty = rand.Next(0, 2);
+string result = coin_probabilty >= 1 ? "heads" : "tails";
+Console.WriteLine($"The coin landed on {result}");
+
+
+
+
+
