dd25eb174a56cadeb870e1986f0e7d6dc97b92c7
RKIddle <asiatutors@gmail.com>
2023-10-10 10:04:36 +0700

    Variable Scope Challenge

---
+++ Variable_scope/Variable-scope-challenge/Program.cs
@@ -0,0 +1,17 @@
+﻿// Variable Scope Challenge
+// Author: Richard Kiddle
+// Date: 10/10/2023
+
+
+int[] numbers = { 4, 8, 15, 16, 23, 42 };
+int total = 0;
+foreach (int number in numbers)
+{
+    total += number;
+
+    if (number == 42)
+       Console.WriteLine("Set contains 42");
+}
+Console.WriteLine($"Total: {total}");
+
+
