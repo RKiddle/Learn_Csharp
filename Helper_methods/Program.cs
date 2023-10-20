27b350fcc5457cc32f8d5f210eb59177118b7693
RKIddle <asiatutors@gmail.com>
2023-10-20 22:47:15 +0700

    helper methods completed

--- Helper_methods/Program.cs
+++ Helper_methods/Program.cs
@@ -50,4 +50,55 @@ foreach (var pallet in pallets)
     Console.WriteLine($"-- {pallet}");
 }
 */
+/*
+string value = "abc123";
+char[] valueArray = value.ToCharArray();
+Array.Reverse(valueArray);
+// string result = new string(valueArray);
+string result = String.Join(",", valueArray);
+Console.WriteLine(result);
+
+string[] items = result.Split(',');
+foreach (string item in items)
+{
+    Console.WriteLine(item);
+}
+*/
+
+
+string pangram = "The quick brown fox jumps over the lazy dog";
+// reverse the letters of each word in place and display the result
+string[] words = pangram.Split(' ');
+foreach (string word in words)
+{
+    char[] letters = word.ToCharArray();
+    Array.Reverse(letters);
+    string reverse = new string(letters);
+    Console.Write(reverse + " ");
+}
+
+// clear the console
+Console.Clear();
+
+
+string orderStream = "B123,C234,A345,C15,B177,G3003,C235,B179";
+string[] orders = orderStream.Split(',');
+Array.Sort(orders);
+foreach (string order in orders)
+{
+    // check the number of characters in each order
+    int characters = order.Length;
+    if (characters == 4)
+    {
+        Console.WriteLine($"{order}");
+    }
+    else
+    {
+        Console.WriteLine($"{order} - Error");
+    }
+
+}
+
+
+
 
