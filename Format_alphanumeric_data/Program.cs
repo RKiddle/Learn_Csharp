8d20e23e4621f7c8cb2dc89f680db7336ed3884f
RKIddle <asiatutors@gmail.com>
2023-10-20 23:27:06 +0700

    apply string interpolation to a form letter

--- Format_alphanumeric_data/Program.cs
+++ Format_alphanumeric_data/Program.cs
@@ -28,5 +28,60 @@ string yourDiscount = String.Format("You saved {0:C2} off the regular {1:C2} pri
 yourDiscount += $"A discount of {((price2 - salePrice)/price):P2}!"; //inserted
 Console.WriteLine(yourDiscount);
 */
+/*
+int invoiceNumber = 1201;
+decimal productShares = 25.4568m;
+decimal subtotal = 2750.00m;
+decimal taxPercentage = .15825m;
+decimal total = 3185.19m;
 
+Console.WriteLine($"Invoice Number: {invoiceNumber}");
+Console.WriteLine($"   Shares: {productShares:N3} Product");
+Console.WriteLine($"     Sub Total: {subtotal:C}");
+Console.WriteLine($"           Tax: {taxPercentage:P2}");
+Console.WriteLine($"     Total Billed: {total:C}");
+*/
+/*
+string input = "Pad this";
+Console.WriteLine(input.PadLeft(12));
+Console.WriteLine(input.PadRight(12));
+Console.WriteLine(input.PadLeft(12, '-'));
+Console.WriteLine(input.PadRight(12, '-'));
+*/
+/*
+string paymentId = "769C";
+string payeeName = "Mr. Stephen Ortega";
+string paymentAmount = "$5,000.00";
 
+var formattedLine = paymentId.PadRight(6);
+formattedLine += payeeName.PadRight(24);
+formattedLine += paymentAmount.PadLeft(10);
+
+Console.WriteLine("1234567890123456789012345678901234567890");
+Console.WriteLine(formattedLine);
+*/
+
+string customerName = "Ms. Barros";
+
+string currentProduct = "Magic Yield";
+int currentShares = 2975000;
+decimal currentReturn = 0.1275m;
+decimal currentProfit = 55000000.0m;
+
+string newProduct = "Glorious Future";
+decimal newReturn = 0.13125m;
+decimal newProfit = 63000000.0m;
+
+Console.WriteLine($"Dear {customerName},");
+Console.WriteLine($"As a customer of our {currentProduct} offering we are excited to tell you about a new financial product that would dramatically increase your return.");
+
+Console.WriteLine($"Currently, you own {currentShares:N} shares at a return of {currentReturn:P}.");
+Console.WriteLine($"Our new product, {newProduct} offers a return of {newReturn:P}.  Given your volume, your potential profit would be {newProfit:C}.");
+
+Console.WriteLine("Here's a quick comparison:\n");
+
+string comparisonMessage = $"{currentProduct} {currentReturn:P} {currentProfit:C} \n";
+comparisonMessage += $"{newProduct} {newReturn:P} {newProfit:C} \n";
+// Your logic here
+
+Console.WriteLine(comparisonMessage);
