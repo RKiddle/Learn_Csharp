622c9b7492857c4ed94ceb1d4cc0991942a090d4
RKIddle <asiatutors@gmail.com>
2023-10-20 23:03:03 +0700

    format data  1

---
+++ Format_alphanumeric_data/Program.cs
@@ -0,0 +1,32 @@
+﻿Console.Clear();
+/*
+string first = "Hello";
+string second = "World";
+Console.WriteLine($"{first} {second}!");
+Console.WriteLine($"{second} {first}!");
+Console.WriteLine($"{first} {first} {first}!");
+
+decimal price = 123.45m;
+int discount = 50;
+Console.WriteLine($"Price: {price:C} (Save {discount:C})");
+
+decimal measurement = 123456.78912m;
+Console.WriteLine($"Measurement: {measurement:N} units");
+
+Console.WriteLine($"Measurement: {measurement:N4} units");
+
+decimal tax = .36785m;
+Console.WriteLine($"Tax rate: {tax:P2}");
+// Tax rate: 36.79 %
+
+
+decimal price2 = 67.55m;
+decimal salePrice = 59.99m;
+
+string yourDiscount = String.Format("You saved {0:C2} off the regular {1:C2} price. ", (price2 - salePrice), price2);
+
+yourDiscount += $"A discount of {((price2 - salePrice)/price):P2}!"; //inserted
+Console.WriteLine(yourDiscount);
+*/
+
+
